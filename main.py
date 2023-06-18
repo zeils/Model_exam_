e5f35af31546d1c62aad73cf9908187e2def1f4d
zeils <61564061+zeils@users.noreply.github.com>
2023-06-18 14:01:48 +0300

    done

--- main.py
+++ main.py
@@ -1,26 +1,76 @@
+import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
-x=np.array([1,2,3,4,5,6,7,8,9,10], dtype=float)
-y=np.array([1,5,6,8,9,10,20,23,60,100], dtype=float)
+from sympy import symbols, diff
 
-#def calc(x,y):
+### Ищем полином
+
+# Определение переменных и коэффициентов
+t = sp.Symbol('t')
+a, b, c, d, e, f = sp.symbols('a b c d e f')
+T = 5
+
+# Определение пятимерной полиномиальной функции временного масштабирования
+polynomial = a * t**5 + b * t**4 + c * t**3 + d * t**2 + e * t + f
+
+# Определение ограничений (constraints)
+constraints = [
+    sp.Eq(polynomial.subs(t, 0), 0), #задаем что a * 0**5 + b * 0**4 + c * 0**3 + d * 0**2 + e * 0 + f =0, т.е. S(0) = 0
+    sp.Eq(sp.diff(polynomial, t).subs(t, 0), 0), #задаем что 5a * 0**4 + 4b * 0**3 + 3c * 0**2 + 2d * 0**1 + e = 0, т.е. S'(0) = 0
+    sp.Eq(sp.diff(polynomial, t, 2).subs(t, 0), 0), #задаем что 20a * 0**3 + 12b * 0**2 + 6c * 0**1 + 2d= 0, т.е. S''(0) = 0
+    sp.Eq(polynomial.subs(t, T), 1), #задаем что a * T**5 + b * T**4 + c * T**3 + d * T**2 + e * T + f =1, т.е. S(T) = 1
+    sp.Eq(sp.diff(polynomial, t).subs(t, T), 0), #задаем что 5a * T**4 + 4b * T**3 + 3c * T**2 + 2d * T**1 + e = 0, т.е. S'(T) = 0
+    sp.Eq(sp.diff(polynomial, t, 2).subs(t, T), 0) #задаем что 20a * T**3 + 12b * T**2 + 6c * T**1 + 2d= 0, т.е. S''(T) = 0
+]
+
+# Решение системы уравнений
+solution = sp.solve(constraints, (a, b, c, d, e, f))
+# Подстановка решений в полиномиальную функцию
+polynomial = polynomial.subs(solution)
+# Вывод полиномиальной функции
+print(polynomial)
+print(solution[a])
+
+Answer = solution[a] * t**5 + solution[b] * t**4 + solution[c] * t**3 + solution[d] * t**2 + solution[e] * t + solution[f]
+
+### Построим график траектории
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = solution[a] * t**5 + solution[b] * t**4 + solution[c] * t**3 + solution[d] * t**2 + solution[e] * t + solution[f]
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График траектории')
+plt.grid(True)
+plt.show()
 
 
+### Построим график скорости
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = 5*solution[a] * t**4 + 4*solution[b] * t**3 + 3*solution[c] * t**2 + 2*solution[d] * t + solution[e] * t
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График скорости')
+plt.grid(True)
+plt.show()
 
-def lagranz(x,y,t):
-    z=0
-    for j in range(len(y)):
-        p1=1; p2=1
-        for i in range(len(x)):
-            if i==j:
-                p1=p1*1; p2=p2*1   
-            else: 
-                p1=p1*(t-x[i])
-                p2=p2*(x[j]-x[i])
-        z=z+y[j]*p1/p2
-    return z
-xnew=np.linspace(np.min(x),np.max(x),100)
-ynew=[lagranz(x,y,i) for i in xnew]
-plt.plot(x,y,'o',xnew,ynew)
+### Построим график ускорения
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = 20*solution[a] * t**3 + 12*solution[b] * t**2 + 6*solution[c] * t + 2*solution[d] 
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График ускорения')
+plt.grid(True)
+plt.show()
+
+
+### Построим график 3й производной
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = 60*solution[a] * t**2 + 24*solution[b] * t + 6*solution[c] 
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График 3й производной')
 plt.grid(True)
 plt.show()
--- test.py
+++ test.py
@@ -1,28 +1,76 @@
-def lagrange_interpolation(x_values, y_values):
-    n = len(x_values)
-    coefficients = []
-    
-    for i in range(n):
-        numerator = [] # массив для хранения числителя дроби в формуле
-        denominator = 1 # начальный знаменатель дроби в формуле
-        
-        for j in range(n):
-            if i != j:
-                numerator.append('(x - {})'.format(x_values[j])) # добавляем в массив часть числителя формулы
-                denominator *= (x_values[i] - x_values[j]) # умножаем знаменательн на разность xi и xj
-        
-        coefficient = '*'.join(numerator) + ' / ' + str(denominator) # обьединяем все элементы массива числителей numerator и делим на знаменатель denominator
-        coefficients.append('(' + str(y_values[i]) + ')*' + coefficient) # добавляем полученную строку в результирующий массив коэффициентов
-    
-    return coefficients
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import symbols, diff
+
+### Ищем полином
+
+# Определение переменных и коэффициентов
+t = sp.Symbol('t')
+a, b, c, d, e, f = sp.symbols('a b c d e f')
+T = 5
+
+# Определение пятимерной полиномиальной функции временного масштабирования
+polynomial = a * t**5 + b * t**4 + c * t**3 + d * t**2 + e * t + f
+
+# Определение ограничений (constraints)
+constraints = [
+    sp.Eq(polynomial.subs(t, 0), 0), #задаем что a * 0**5 + b * 0**4 + c * 0**3 + d * 0**2 + e * 0 + f =0, т.е. S(0) = 0
+    sp.Eq(sp.diff(polynomial, t).subs(t, 0), 0), #задаем что 5a * 0**4 + 4b * 0**3 + 3c * 0**2 + 2d * 0**1 + e = 0, т.е. S'(0) = 0
+    sp.Eq(sp.diff(polynomial, t, 2).subs(t, 0), 0), #задаем что 20a * 0**3 + 12b * 0**2 + 6c * 0**1 + 2d= 0, т.е. S''(0) = 0
+    sp.Eq(polynomial.subs(t, T), 1), #задаем что a * T**5 + b * T**4 + c * T**3 + d * T**2 + e * T + f =1, т.е. S(T) = 1
+    sp.Eq(sp.diff(polynomial, t).subs(t, T), 0), #задаем что 5a * T**4 + 4b * T**3 + 3c * T**2 + 2d * T**1 + e = 0, т.е. S'(T) = 0
+    sp.Eq(sp.diff(polynomial, t, 2).subs(t, T), 0) #задаем что 20a * T**3 + 12b * T**2 + 6c * T**1 + 2d= 0, т.е. S''(T) = 0
+]
+
+# Решение системы уравнений
+solution = sp.solve(constraints, (a, b, c, d, e, f))
+# Подстановка решений в полиномиальную функцию
+polynomial = polynomial.subs(solution)
+# Вывод полиномиальной функции
+print(polynomial)
+print(solution[a])
+
+Answer = solution[a] * t**5 + solution[b] * t**4 + solution[c] * t**3 + solution[d] * t**2 + solution[e] * t + solution[f]
+
+### Построим график траектории
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = solution[a] * t**5 + solution[b] * t**4 + solution[c] * t**3 + solution[d] * t**2 + solution[e] * t + solution[f]
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График траектории')
+plt.grid(True)
+plt.show()
 
 
-#x = [1, 2, 3, 4, 5, 6, 7, 8] #для 7го
-#y = [4, 1, 3, 2, 6, 9, 200, 30] 
+### Построим график скорости
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = 5*solution[a] * t**4 + 4*solution[b] * t**3 + 3*solution[c] * t**2 + 2*solution[d] * t + solution[e] * t
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График скорости')
+plt.grid(True)
+plt.show()
 
-x = [1, 2, 3, 4, 5, 6] #для 5го
-y = [4, 1, 3, 2, 6, 9]
+### Построим график ускорения
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = 20*solution[a] * t**3 + 12*solution[b] * t**2 + 6*solution[c] * t + 2*solution[d] 
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График ускорения')
+plt.grid(True)
+plt.show()
 
-coefficients = lagrange_interpolation(x, y)
-formula = ' + '.join(coefficients) #обьединяем все коэффициенты и получаем формулу
-print(formula)
+
+### Построим график 3й производной
+t = np.linspace(-100, 100, 1000) # Создание массива значений t от -100 до 100
+S = 60*solution[a] * t**2 + 24*solution[b] * t + 6*solution[c] 
+plt.plot(t, S)
+plt.xlabel('t')
+plt.ylabel('S')
+plt.title('График 3й производной')
+plt.grid(True)
+plt.show()
